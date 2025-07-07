7483e052a19cbba3fbfb2910568dc17cf04be448
LouisLat <louis@hozana.org>
2025-07-07 23:52:13 +0200

    Update redaction.py

--- redaction.py
+++ redaction.py
@@ -13,7 +13,6 @@ import unicodedata
 from google.oauth2 import service_account
 from google.ads.googleads.client import GoogleAdsClient
 from google.ads.googleads.errors import GoogleAdsException
-from google.ads.googleads.v14.services.types.keyword_plan_idea_service import GenerateKeywordIdeaRequest
 
 SERP_API_KEY = st.secrets["serp_api_key"]
 MAGISTERIUM_API_KEY = st.secrets["magisterium_api_key"]
